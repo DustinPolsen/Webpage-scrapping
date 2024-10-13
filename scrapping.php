3f1f2813ac7d440834559420e2aa4f4c696abfe8
Mihailo Vojinovic <mihailonvojinovic@gmail.com>
2024-10-13 17:30:01 +0200

    Delete some lines

--- scrapping.php
+++ scrapping.php
@@ -1,12 +1,6 @@
 <?php
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, 'https://trickbd.com/');
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$html = curl_exec($ch);
-
-$dom = new DOMDocument();
-@$dom->loadHTML($html);
 
 $h2s = $dom->getElementsByTagName('li');
 $h2 = $h2s->item(0);
